4a91d480faa941436ae40200087de23fc9c5f000
julio <julio.desenvolvedor.java@gmail.com>
2018-08-12 20:26:03 -0300

    devdojo aula 14 - Tratamento de erros em REST pt 05 - Padronizando todos os erros

--- src/main/java/br/com/devdojo/handler/RestExceptionHandler.java
+++ src/main/java/br/com/devdojo/handler/RestExceptionHandler.java
@@ -4,19 +4,23 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import br.com.devdojo.error.ErrorDetails;
 import br.com.devdojo.error.ResourceNotFoundDetails;
 import br.com.devdojo.error.ResourceNotFoundException;
 import br.com.devdojo.error.ValidationErrorDetails;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfException){
@@ -32,8 +36,9 @@ public class RestExceptionHandler {
 		return new ResponseEntity<>(rnfDetails, HttpStatus.NOT_FOUND);
 	}
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException manvException){
+	@Override
+	public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manvException,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		List<FieldError> fieldErrors = manvException.getBindingResult().getFieldErrors();
 		String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
 		String fieldMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
@@ -49,5 +54,22 @@ public class RestExceptionHandler {
 				.build();
 		
 		return new ResponseEntity<>(manvDetails, HttpStatus.BAD_REQUEST);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
+			Object body,
+			HttpHeaders headers,
+			HttpStatus status,
+			WebRequest request) {
+		ErrorDetails errorDetails = ErrorDetails.Builder
+				.newBuilder()
+				.timestamp(new Date().getTime())
+				.status(status.value())
+				.title("Internal Exception")
+				.detail(ex.getMessage())
+				.developerMessage(ex.getClass().getName())
+				.build();
+		return new ResponseEntity<>(errorDetails, headers, status);
 	}
 }
