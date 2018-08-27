40ca39f4266b93793866ad1213d4df500fe10ab2
julio <julio.desenvolvedor.java@gmail.com>
2018-08-27 06:12:14 -0300

    devdojo aula 20 - Exemplo de conexão com Java Puro

--- src/main/java/br/com/devdojo/javaclient/JavaClientTest.java
+++ src/main/java/br/com/devdojo/javaclient/JavaClientTest.java
@@ -13,11 +13,15 @@ public class JavaClientTest {
 		HttpURLConnection connection = null;
 		BufferedReader reader = null;
 		
+		String user = "juliodasilv";
+		String password = "Athena00)";
+		
 		try{
 			URL url= new URL("http://localhost:8080/v1/protected/students/1");
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
-			connection.addRequestProperty("Authorization", "Basic " + encodeUsernamePassword("juliodasilv", "Athena00)"));
+			connection.addRequestProperty("Authorization", "Basic " + encodeUsernamePassword(user, password));
+			System.out.println(encodeUsernamePassword(user, password));
 			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			StringBuilder jsonSB =new StringBuilder();
 			String line;
